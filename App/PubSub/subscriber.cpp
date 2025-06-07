dad1b9accb8fda98fa4c664d5788fc7c2d342428
Phuong Dao <phuong018@gmail.com>
2025-06-07 16:09:24 +0200

    Add port option for WeatherSubscriber

--- App/PubSub/subscriber.cpp
+++ App/PubSub/subscriber.cpp
@@ -1,20 +1,31 @@
+#include <cstdlib>
 #include <sstream>
 
 #include <spdlog/spdlog.h>
 #include <zmq.hpp>
 
 int main( int argc, char *argv[] ) {
+	auto port = 5556;
+
+	if ( argc > 1 && argc != 2 ) {
+		exit( EXIT_FAILURE );
+	}
+
+	if ( argc == 2 ) {
+		port = std::stoi( argv[ 1 ] );
+	}
+
 	zmq::context_t zmqContext;
 	spdlog::info( "Collecting updates from weather server...\n" );
 	zmq::socket_t subscriber{ zmqContext, zmq::socket_type::sub };
-	subscriber.connect( "tcp://localhost:5556" );
+	subscriber.connect( std::format( "tcp://localhost:{}", port ) );
 
 	// Filter only for zipCode = 10001. This step is always needed. Otherwise we
 	// will receive nothing
-	subscriber.set( zmq::sockopt::subscribe, "10001");
+	subscriber.set( zmq::sockopt::subscribe, "10001" );
 
-    // Try to set another subscription
-    subscriber.set(zmq::sockopt::subscribe, "10002");
+	// Try to set another subscription
+	subscriber.set( zmq::sockopt::subscribe, "10002" );
 
 	while ( 1 ) {
 		uint32_t zipCode;
