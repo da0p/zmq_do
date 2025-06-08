d276ce594225712103f04452fce99ed9714e6900
Phuong Dao <phuong018@gmail.com>
2025-06-08 16:33:00 +0200

    Format publisher code

--- App/PubSub/publisher.cpp
+++ App/PubSub/publisher.cpp
@@ -7,7 +7,7 @@
 
 int main( int argc, char *argv[] ) {
 
-	zmq::context_t zmqContex{1};
+	zmq::context_t zmqContex{ 1 };
 	zmq::socket_t publisher{ zmqContex, zmq::socket_type::pub };
 	publisher.bind( "tcp://*:5556" );
 	publisher.bind( "ipc://weather.ipc" );
@@ -17,13 +17,13 @@ int main( int argc, char *argv[] ) {
 		auto zipCode = std::format( "{:{}d}", random.generate(), 5 );
 		auto temperature = std::format( "{:{}.{}f}", static_cast<float>( random.generate() ) / 1000, 3, 1 );
 		auto humidity = std::format( "{:{}.{}f}", static_cast<float>( random.generate() ) / 1000, 3, 1 );
-        std::string text;
+		std::string text;
 		std::format_to( std::back_inserter( text ), "{} {} {}", zipCode, temperature, humidity );
-		zmq::message_t notification{ text.length()};
-        std::memcpy(notification.data(), text.data(), text.length());
-        spdlog::info("Publish weather update. Zip code = {}, temperature = {}, humidity = {}", zipCode, temperature, humidity);
+		zmq::message_t notification{ text.length() };
+		std::memcpy( notification.data(), text.data(), text.length() );
+		spdlog::info( "Publish weather update. Zip code = {}, temperature = {}, humidity = {}", zipCode, temperature, humidity );
 		publisher.send( notification, zmq::send_flags::none );
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
 	}
 
 	return 0;
