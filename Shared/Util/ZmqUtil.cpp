b8346d852e8e70507718fb941b1693481c93873d
Phuong Dao <phuong018@gmail.com>
2025-06-09 10:21:49 +0200

    Add dump method in utility

--- Shared/Util/ZmqUtil.cpp
+++ Shared/Util/ZmqUtil.cpp
@@ -1,5 +1,7 @@
 #include "ZmqUtil.h"
 
+#include <format>
+#include <iostream>
 #include <zmq.hpp>
 
 namespace ZmqUtil {
@@ -13,5 +15,39 @@ namespace ZmqUtil {
 		zmq::message_t message;
 		[[maybe_unused]] auto _ = socket.recv( message, flag );
 		return message.to_string();
+	}
+
+	void dump( zmq::socket_t &socket ) {
+		while ( 1 ) {
+			zmq::message_t message;
+			auto size = socket.recv( message, zmq::recv_flags::none );
+			if ( size < 0 ) {
+				std::cerr << "Error in receiving message." << std::endl;
+				return;
+			}
+			std::string raw( static_cast<char *>( message.data() ), size.value() );
+			bool isText{ true };
+			for ( size_t i = 0; i < size.value(); i++ ) {
+				if ( raw[ i ] < 32 || raw[ i ] > 127 ) {
+					isText = false;
+					break;
+				}
+			}
+			if ( isText ) {
+				std::cout << std::format( "[{:<}]	{}\n", size.value(), raw );
+			} else {
+				auto out = std::format( "[{:<}]	", size.value() );
+				for ( size_t i = 0; i < size.value(); i++ ) {
+					out += std::format( "{0:x}", raw[ i ] );
+				}
+				out += "\n";
+				std::cout << out;
+			}
+
+			auto more = socket.get( zmq::sockopt::rcvmore );
+			if ( !more ) {
+				break;
+			}
+		}
 	}
 }
--- Shared/Util/ZmqUtil.h
+++ Shared/Util/ZmqUtil.h
@@ -1,10 +1,12 @@
 #ifndef ZMQ_UTIL_H_
 #define ZMQ_UTIL_H_
+
 #include <zmq.hpp>
 
 namespace ZmqUtil {
 	void sendString( zmq::socket_t &socket, std::string_view text, zmq::send_flags flag = zmq::send_flags::none );
 	[[nodiscard]] std::string recvString( zmq::socket_t &socket, zmq::recv_flags flag = zmq::recv_flags::none );
+	void dump( zmq::socket_t &socket );
 };
 
 #endif
